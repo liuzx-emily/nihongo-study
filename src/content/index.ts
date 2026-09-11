ca82a8c4c4c2d727796f06695887909b226ca63d
liuzx-emily <liuzixuan_emily@outlook.com>
2026-09-11 22:25:48 +0800

    feat: add Oda Nobunaga Hieizan study article

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,11 +1,15 @@
 import type { StudyArticle } from '../types'
 import badJapaneseLesson from './2026-09-10-bad-japanese-lesson'
 import japanesePronunciation from './2026-09-10-japanese-pronunciation'
+import odaNobunagaHieizan from './2026-09-11-oda-nobunaga-hieizan'
 import goodJapaneseLesson from './good-japanese-lesson'
 
-export const articles: StudyArticle[] = [japanesePronunciation, badJapaneseLesson, goodJapaneseLesson].sort((a, b) =>
-  b.date.localeCompare(a.date),
-)
+export const articles: StudyArticle[] = [
+  odaNobunagaHieizan,
+  japanesePronunciation,
+  badJapaneseLesson,
+  goodJapaneseLesson,
+].sort((a, b) => b.date.localeCompare(a.date))
 
 export function getArticle(slug: string) {
   return articles.find((article) => article.slug === slug)
