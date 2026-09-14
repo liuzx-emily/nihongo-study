3cc4d44622288049042445fc4e112bf8a4158c4a
liuzx-emily <liuzx-emily@users.noreply.github.com>
2026-09-14 17:35:20 +0800

    feat: add YUYU language study article

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,10 +1,12 @@
 import type { StudyArticle } from '../types'
+import languageNotFullyConveyed from './2026-09-14-language-not-fully-conveyed'
 import badJapaneseLesson from './2026-09-10-bad-japanese-lesson'
 import japanesePronunciation from './2026-09-10-japanese-pronunciation'
 import odaNobunagaHieizan from './2026-09-14-oda-nobunaga-hieizan'
 import goodJapaneseLesson from './good-japanese-lesson'
 
 export const articles: StudyArticle[] = [
+  languageNotFullyConveyed,
   odaNobunagaHieizan,
   japanesePronunciation,
   badJapaneseLesson,
